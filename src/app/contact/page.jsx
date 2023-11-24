77b30eeface7765a340dd754e8b6045f2ca66956
Nguyen Hoang Phuong Nam <nhpn2003@gmail.com>
2023-11-24 11:18:24 +0700

    update

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+'use client'
+import React, { useRef, useState } from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
 import Button from '@/components/button/Button'
-
+import emailjs from '@emailjs/browser'
 
 export const metadata = {
   title: "Namne Dev Contact",
@@ -10,6 +11,30 @@ export const metadata = {
 }
 
 const Contact = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  var templateParams = {
+    name: name,
+    email: email,
+    message: message
+  };
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.send('service_seylfxs', 'template_91s3f5s', templateParams, '3wmfQv_HlL-lVqKay')
+      .then(function (response) {
+        console.log('SUCCESS!', response.status, response.text);
+      }, function (error) {
+        console.log('FAILED...', error);
+      });
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Let's Keep in Touch</h1>
@@ -23,11 +48,11 @@ const Contact = () => {
           />
         </div>
 
-        <form className={styles.form}>
-          <input type="text" placeholder='Name' className={styles.input} />
-          <input type="text" placeholder='Email' className={styles.input}/>
-          <textarea className={styles.textarea} cols="30" rows="10"></textarea>
-          <Button url="#" text="Send" />
+        <form ref={form} onSubmit={sendEmail} className={styles.form}>
+          <input value={name} onChange={(e) => setName(e.target.value)} name='user_name' type="text" placeholder='Name' className={styles.input} />
+          <input value={email} onChange={(e) => setEmail(e.target.value)} name='user_email' type="text" placeholder='Email' className={styles.input} />
+          <textarea value={message} onChange={(e) => setMessage(e.target.value)} name='message' className={styles.textarea} cols="30" rows="10"></textarea>
+          <Button text="Send" />
         </form>
       </div>
     </div>
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import styles from "./button.module.css";
 import Link from "next/link";
 
-const Button = ({ text, url }) => {
+const Button = ({ text }) => {
   return (
-    <Link href={url}>
-      <button className={styles.container}>{text}</button>
-    </Link>
+    <button value='Send' type="submit" className={styles.container}>{text}</button>
   );
 };
 
